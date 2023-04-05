ac28611d6b344d8bcdebdd3def2c0fc6e23f9de3
liveisliiife <127602972+liveisliiife@users.noreply.github.com>
2023-04-05 23:32:38 +0300

    Update most_common_x_words.py

--- most_common_x_words.py
+++ most_common_x_words.py
@@ -6,7 +6,11 @@ if not os.path.isfile(name):
     exit()
 
 file = open(name)
-x = int(input("How many words would you like me to find the most common words? "))
+
+x = int(input("How many of the top most frequently occurring words would you like to find? "))
+if x<1:
+    x = 1
+
 for line in file:
     words = line.split()
     for word in words:
@@ -19,6 +23,9 @@ for k,v in my_dict.items():
 
 lst = sorted(lst,reverse=True)
 
+if len(lst) < x:
+    x = len(lst)
+    print("We have just",x,"different words.")
 # most common x words
 for val,key in lst[:x]:
     print(key,val)
